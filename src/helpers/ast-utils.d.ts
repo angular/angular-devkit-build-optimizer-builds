c8a1a648b91f364a202dfc446408d401b56c7ecb
Angular Builds <circleci@angular.io>
2019-01-23 18:28:58 +0000

    e12adf4 refactor: create helper functions for pure comments

--- src/helpers/ast-utils.d.ts
+++ src/helpers/ast-utils.d.ts
@@ -7,3 +7,5 @@
  */
 import * as ts from 'typescript';
 export declare function collectDeepNodes<T extends ts.Node>(node: ts.Node, kind: ts.SyntaxKind): T[];
+export declare function addPureComment<T extends ts.Node>(node: T): T;
+export declare function hasPureComment(node: ts.Node): boolean;
