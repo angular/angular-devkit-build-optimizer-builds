d01290a528a463b64f9224ef9c442ca1b0581f33
Angular Builds <circleci@angular.io>
2021-02-18 12:37:49 +0000

    c9868325a build: exclude workspace json test files from core package

--- src/build-optimizer/webpack-plugin.d.ts
+++ src/build-optimizer/webpack-plugin.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { Compiler } from 'webpack';
+import { Compiler, WebpackFourCompiler } from 'webpack';
 export declare class BuildOptimizerWebpackPlugin {
-    apply(compiler: Compiler): void;
+    apply(compiler: Compiler | WebpackFourCompiler): void;
 }
